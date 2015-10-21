68148ab2bd0e900f137c8a3ef5d0ff5aafc62294
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-10-21 16:08:52 +0200

    5

--- contact.php
+++ contact.php
@@ -1,32 +1,37 @@
 <?php
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+//$field_message = $_POST['cf_message'];
 
-$mail_to = 'newthinks@live.com';
-$subject = 'Message from a site visitor '.$field_name;
+$mail_to = 'wojtek.sasiela@gmail.com';
+$subject = 'Wiadomość od uczestnika '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
+//$body_message .= 'Message: '.$field_message;
+$body_message .= 'Wiadomość: Chciałbym/ałabym dołączyć do Waszej konferencji. Pozdrawiam :)';
+
 
 $headers = 'From: '.$field_email."\r\n";
 $headers .= 'Reply-To: '.$field_email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
+//$mail_status = mail("wojtek.sasiela@gmail.com", "temat wiadomosci", "tresc wiadomosci");
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
+		alert('Dziekujemy za wiadomosc. Skontaktujemy sie wkrotce.');
 		window.location = 'index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to newthinks@live.com');
+		alert('Nie udalo sie wyslać wiadomości. prosze wyslij recznie na konferencja.ss@gmail.com');
 		window.location = 'index.html';
 	</script>
 <?php
 }
+error_reporting(E_ALL);
+ini_set('display_errors', 1); 
 ?>
